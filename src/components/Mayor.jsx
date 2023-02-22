10367db99fe289ba9643a064468a515ad5a7a8e8
CrzRain <dylancruickshank0@gmail.com>
2023-02-22 12:59:43 +0000

    Fetch API data and save into state

--- src/components/Mayor.jsx
+++ src/components/Mayor.jsx
@@ -2,5 +2,11 @@ import React, {useState, useEffect} from "react";
 
 
 export default function Mayor() {
-    
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("api.hypixel.net/resources/skyblock/election")
+        .then(res => res.json())
+        .then(data => setData(data.current))
+    }, [])
 }
