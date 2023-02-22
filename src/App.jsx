8da1394eb113f27ddf6340f9e37d4ed5e907ca3e
CrzRain <dylancruickshank0@gmail.com>
2023-02-22 13:44:47 +0000

    Render mayor component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/header'
+import Mayor from './components/Mayor'
 
 function App() {
 
   return (
     <div className='app'>
       <Header />
+      <Mayor />
     </div>
   )
 }
